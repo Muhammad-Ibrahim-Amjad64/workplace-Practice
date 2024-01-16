ae194dc12afdb9619077b92b71ba2c2166a62536
Muhammad-Ibrahim-Amjad64 <123ibrahim7890@gmail.com>
2024-01-16 15:16:21 +0500

    nextjs

---
+++ NEXTJS LEARNING/NOTES.js
@@ -0,0 +1,12 @@
+//  FOLDER STRUCTURE
+// ---------------------------------------------------------------------------
+
+// jsconfig.json 
+//  in that folder we can define defaul traversing path
+// @ can be user to directly jump to src folder 
+
+
+
+// APP ROUTER 
+// --------------------------------------------------------------------
+// we can traverse across pages by using "App" folder    -- for routing purposes 
